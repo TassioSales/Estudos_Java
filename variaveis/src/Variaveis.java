4006a8260ef7d644434dc60efcd05e96bd856aed
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-05-30 19:01:38 -0300

    variaveis

---
+++ variaveis/src/Variaveis.java
@@ -0,0 +1,11 @@
+public class Variaveis {
+    public static void main(String[] args){
+
+        int idade = 20;
+        String nome ="Tassio";
+        String nomeDoPet = "Swam";
+        System.out.println("Idade = " + idade);
+        System.out.println("Nome  = " + nome);
+        System.out.println("Nome do Pet = " + nomeDoPet);
+    }
+}
