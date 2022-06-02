ee10b48a57e141a7d71ed6886c2b63c4d1c2f50b
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-06-01 23:04:01 -0300

    add Operadores

---
+++ OperadoresAritimeticos/src/OperadoresLogicos.java
@@ -0,0 +1,26 @@
+public class OperadoresLogicos {
+    public static void main(String[] args){
+
+        int valor1 = 1;
+        int valor2 = 2;
+
+        boolean resultado1 =  (valor1 == 1) && (valor2 == 2);
+        System.out.println("Valor1 e 1 AND valor2 e 2 - resultado: " + resultado1);
+
+        boolean resultado2 =  (valor1 == 1) || (valor2 == 2);
+        System.out.println("Valor1 e 1 AND valor2 e 2 - resultado: " + resultado2);
+
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        System.out.println(verdadeiro && falso);
+        System.out.println(verdadeiro || falso);
+        System.out.println(verdadeiro ^ falso);
+        System.out.println(!verdadeiro && falso);
+        System.out.println(!verdadeiro || falso);
+
+
+
+
+    }
+}
